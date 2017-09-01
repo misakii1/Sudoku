c9c38cfe70941e6a6f7323811878c01655d6e376
tim <tim@timdemac-pro.lan>
2017-09-01 15:31:06 +0800

    Initial Commit

---
+++ 0326/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  0326
+//
+//  Created by tim on 2017/9/1.
+//  Copyright © 2017年 tim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
